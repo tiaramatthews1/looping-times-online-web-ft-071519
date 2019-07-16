4971cfd8f1966696185fc958f6de252884a77150
Tiara Matthews <tiaramatthews96@yahoo.com>
2019-07-16 23:41:02 +0000

    Automatically backed up by Learn

--- times.rb
+++ times.rb
@@ -1,6 +1,6 @@
 def using_times(string)
 	7.times do
-	  puts "Wingardium Leviosa"https://learn.co/tracks/online-software-engineering-structured/procedural-ruby/looping/times#
+	  puts "Wingardium Leviosa"
 end
 
 
